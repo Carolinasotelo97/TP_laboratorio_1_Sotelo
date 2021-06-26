ba73590d4d3a68e752cfacdc5cbe88ca25372279
carolinasotelo97 <carolinasotelo97@gmail.com>
2021-06-26 09:28:08 -0300

    final

--- primerTrabajoSotelo/src/primerTrabajoSotelo.c
+++ primerTrabajoSotelo/src/primerTrabajoSotelo.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : primerTrabajoSotelo.c
- Author      : 
+ Author      : Sotelo Carolina
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
